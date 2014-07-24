7b7d6e1bbb175d11f0d8e18d58aa9e38201eb5a8
zhangbook <395836998@qq.com>
2014-07-25 00:05:48 +0800

    util

---
+++ src/com/zhanglian2010/util/FileUtils.java
@@ -0,0 +1,84 @@
+package com.zhanglian2010.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.os.Environment;
+
+public class FileUtils {
+	private String SDCardRoot;
+
+	public FileUtils() {
+		// �õ���ǰ�ⲿ�洢�豸��Ŀ¼
+		SDCardRoot = Environment.getExternalStorageDirectory()
+				.getAbsolutePath()
+				+ File.separator;
+	}
+
+	/**
+	 * ��SD���ϴ����ļ�
+	 * 
+	 * @throws IOException
+	 */
+	public File createFileInSDCard(String fileName, String dir)
+			throws IOException {
+		File file = new File(SDCardRoot + dir + File.separator + fileName);
+		System.out.println("file---->" + file);
+		file.createNewFile();
+		return file;
+	}
+
+	/**
+	 * ��SD���ϴ���Ŀ¼
+	 * 
+	 * @param dirName
+	 */
+	public File creatSDDir(String dir) {
+		File dirFile = new File(SDCardRoot + dir + File.separator);
+		System.out.println(dirFile.mkdirs());
+		return dirFile;
+	}
+
+	/**
+	 * �ж�SD���ϵ��ļ����Ƿ����
+	 */
+	public boolean isFileExist(String fileName, String path) {
+		File file = new File(SDCardRoot + path + File.separator + fileName);
+		return file.exists();
+	}
+
+	/**
+	 * ��һ��InputStream��������д�뵽SD����
+	 */
+	public File write2SDFromInput(String path, String fileName,
+			InputStream input) {
+
+		File file = null;
+		OutputStream output = null;
+		try {
+			creatSDDir(path);
+			file = createFileInSDCard(fileName, path);
+			output = new FileOutputStream(file);
+			byte buffer[] = new byte[4 * 1024];
+			int temp;
+			while ((temp = input.read(buffer)) != -1) {
+				output.write(buffer, 0, temp);
+			}
+			output.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				output.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return file;
+	}
+
+
+}
